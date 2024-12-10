79cc3fa4b2f91b729303f59687574813a7b57c47
JMN327 <173676271+JMN327@users.noreply.github.com>
2024-12-10 14:39:00 +0000

    Clickable drop down complete

--- src/drop-down.js
+++ src/drop-down.js
@@ -1,42 +1,74 @@
 import "./drop-down.css";
 
-let windowEventListenerInitiated = false;
-
-export function MakeDropDownHover(button, list) {
+export function hoverable(
+  button = document.createElement("div"),
+  list = document.createElement("div")
+) {
   if (!(button || list)) {
     return;
   }
 
   button.classList.add("drop-down-hover__button");
   list.classList.add("drop-down-hover__list");
+
+  handleListClicks();
 }
 
-export function MakeDropDownClick(button = document.createElement("div"), list = document.createElement("div")) {
+export function clickable(
+  button = document.createElement("div"),
+  list = document.createElement("div")
+) {
   if (!(button || list)) {
     return;
   }
-
   hideList();
 
   button.classList.add("drop-down-click__button");
+  button.classList.add("button-hide");
   list.classList.add("drop-down-click__list");
+  list.classList.add("list-hide");
 
-  if (!windowEventListenerInitiated) {
-    window.addEventListener("mousedown", (event) => hideList(event));
-    windowEventListenerInitiated = true;
-  }
+  window.addEventListener("mousedown", (event) => checkClick(event.target));
 
-  button.addEventListener("mouseup", showList);
+  button.addEventListener("mouseup", toggleListVisibility);
 
   function showList() {
-    console.log("showing list")
-    list.classList.remove("hide")
-    list.classList.add("show")
+    if (!list.classList.contains("list-hide")) {
+      return;
+    }
+    console.log("showing list");
+    list.classList.remove("list-hide");
+    list.classList.add("list-show");
+    button.classList.remove("button-hide");
+    button.classList.add("button-show");
   }
 
-  function hideList(event) {
-    list.classList.remove("show")
-    list.classList.add("hide")
-    
+  function hideList() {
+    if (!list.classList.contains("list-show")) {
+      return;
+    }
+    console.log("hiding list");
+    list.classList.remove("list-show");
+    list.classList.add("list-hide");
+    button.classList.remove("button-show");
+    button.classList.add("button-hide");
   }
+
+  function toggleListVisibility() {
+    list.classList.contains("list-hide") ? showList() : hideList();
+  }
+
+  function checkClick(target) {
+    if (!target.closest(".drop-down-click__button")) {
+      hideList();
+    }
+    if (target.classList.contains(".drop-down-click__button")) {
+      console.log(`dropdown clicked`);
+      hideList();
+    }
+  }
+
+  handleListClicks();
 }
+
+function handleListClicks() {}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./styles.css";
-import "./drop-down.css";
 import storageAvailable from "./local-storage.js";
-import { MakeDropDownHover, MakeDropDownClick } from "./drop-down.js";
+import * as MakeDropDown from "./drop-down.js";
 
 console.log("Hello World!)");
 console.log(`Storage available: ${storageAvailable("localStorage")}`);
@@ -11,13 +10,13 @@ const list1 = document.querySelector(".list1");
 const button2 = document.querySelector(".item2");
 const list2 = document.querySelector(".list2");
 
-MakeDropDownHover(button1, list1);
-MakeDropDownHover(button2, list2);
+MakeDropDown.hoverable(button1, list1);
+MakeDropDown.hoverable(button2, list2);
 
 const button3 = document.querySelector(".item3");
 const list3 = document.querySelector(".list3");
 const button4 = document.querySelector(".item4");
 const list4 = document.querySelector(".list4");
 
-MakeDropDownClick(button3, list3);
-MakeDropDownClick(button4, list4);
+MakeDropDown.clickable(button3, list3);
+MakeDropDown.clickable(button4, list4);
